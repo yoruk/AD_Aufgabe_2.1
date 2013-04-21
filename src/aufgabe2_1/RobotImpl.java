01c3cc10d3ea989e805c34c955b20b2f6e131342
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2013-04-21 19:42:45 +0200

    robotcode von mike momentan noch auskommentiert

--- src/aufgabe2_1/RobotImpl.java
+++ src/aufgabe2_1/RobotImpl.java
@@ -36,4 +36,102 @@ public class RobotImpl implements Robot {
 	public void receiveOrder(Map<Item, Integer> order) {
 		this.order = order;
 	}
+	
+//  /**
+//  * Bewegt sich auf die neue position und belegt sie
+//  * @param position zukünftige position
+//  * @return anzahl Roboter auf neuem Feld
+//  */
+// private int moveTo(int positionY, int positionX) {
+//     field[this.positionY][this.positionX].unReg();
+//     this.positionY = positionY;
+//     this.positionX = positionX;
+//     field[positionY][positionX].reg(this);
+//     
+//     return field[positionY][positionX].hasRobots();
+// }
+//
+// /**
+//  * Findet herraus in welche richtung er gehen muss;
+//  * @param destination Wo der Robot hin will
+//  * @return Richtung in die er gehen muss (N, S, W, E)
+//  */
+// private char findWay(int destinationY, int destinationX ) {
+//     if(destinationY < positionY){
+//         return 'N';
+//     } else if (destinationY > positionY){
+//         return 'S';
+//     } else if (destinationX < positionX) {
+//         return 'W';
+//     } else if (destinationX > positionX) {
+//         return 'E';
+//     } else { 
+//         return 'A';
+//     }
+// }
+// 
+// /**
+//  * Prüft ob auf einem Robot ein Feld ist
+//  * 
+//  * @param positionY feld was geprüft werden soll
+//  * @param positionX feld was geprüft werden soll
+//  * @return es wird true zurück gegeben wenn das feld frei ist sonst false
+//  */
+// private boolean fieldFree(int positionY, int positionX) {
+//     if (field[positionY][positionX].robotID() != 0) {
+//         return false;
+//     }
+//     return true;
+// }
+// 
+// private int crash() {
+//     
+//     return 0;
+// }
+// 
+// public int id() {
+//     return ID;
+// }
+// 
+// public boolean giveOrder(Order order) {
+//     if(this.order == null) {
+//         this.order = order;
+//         return true;
+//     }
+//    return false;
+// }
+// 
+// public void action() {
+//     
+//     if(order != null) {
+//         switch(findWay(order.getItem().productPosY(),order.getItem().productPosX())) {
+//         case 'N':   if(fieldFree(positionY-1,positionX)){
+//                         moveTo(positionY-1,positionX);
+// //                        System.out.printf("N");
+//                     }
+//             break;
+//         case 'S':   if(fieldFree(positionY+1,positionX)){
+//                         moveTo(positionY+1,positionX);
+// //                        System.out.printf("S");
+//                         
+//                     }
+//             break;
+//         case 'W':   if(fieldFree(positionY,positionX-1)) {
+//                         moveTo(positionY,positionX-1);
+// //                        System.out.printf("W");
+//                     }
+//             break;
+//         case 'E':   if(fieldFree(positionY,positionX+1)) {
+//                         moveTo(positionY,positionX+1);
+// //                        System.out.printf("E");
+//                     }
+//             break;
+//         case 'A':   //if( Schon da)
+//                     // items laden
+// //                    System.out.printf("Error");
+//             break;
+//         }
+//     }
+// }
 }
+
